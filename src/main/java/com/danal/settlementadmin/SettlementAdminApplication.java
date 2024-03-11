08c45c559fea8eef54a7f1f0c76616931fcc4268
Gogoonbuntu <tmddud333@naver.com>
2024-03-11 14:46:30 +0900

    init

---
+++ src/main/java/com/danal/settlementadmin/SettlementAdminApplication.java
@@ -0,0 +1,13 @@
+package com.danal.settlementadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danal/settlementadmin/SettlementAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danal.settlementadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
